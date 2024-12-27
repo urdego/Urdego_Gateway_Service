d43bf8ed103c9e1bb1a3bac3772c94d455a945aa
ooni <gdbsrjsdn@gmail.com>
2024-12-27 16:00:39 +0900

    start project

---
+++ src/main/java/io/urdego/urdego_gateway_service/UrdegoGatewayServiceApplication.java
@@ -0,0 +1,13 @@
+package io.urdego.urdego_gateway_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrdegoGatewayServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UrdegoGatewayServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/urdego/urdego_gateway_service/UrdegoGatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.urdego.urdego_gateway_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrdegoGatewayServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
