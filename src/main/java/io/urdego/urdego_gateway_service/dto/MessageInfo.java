b180cb7e48c2f64c03a3adc86e8b116a47f46c8f
Ban-gilhyeon <bbgiloo98@gmail.com>
2025-01-31 15:39:34 +0900

    HOTFIX : websocket 의존성 제거

--- src/main/java/io/urdego/urdego_gateway_service/dto/MessageInfo.java
+++
@@ -1,8 +0,0 @@
-package io.urdego.urdego_gateway_service.dto;
-
-public record MessageInfo<T>(
-        String type,
-        T payload,
-        String userId
-) {
-}
--- src/main/java/io/urdego/urdego_gateway_service/feign/game/dto/GameServiceFeignClient.java
+++
@@ -1,4 +0,0 @@
-package io.urdego.urdego_gateway_service.feign.game.dto;
-
-public interface GameServiceFeignClient {
-}
