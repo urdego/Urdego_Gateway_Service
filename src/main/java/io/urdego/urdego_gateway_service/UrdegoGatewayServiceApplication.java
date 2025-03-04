974c973bb6a3303ae04ac1231d8e73174c88d7e4
JoungWoo Lee <j_ra1n@naver.com>
2025-03-04 15:31:17 +0900

    Actions 테스트

--- src/main/java/io/urdego/urdego_gateway_service/UrdegoGatewayServiceApplication.java
+++ src/main/java/io/urdego/urdego_gateway_service/UrdegoGatewayServiceApplication.java
@@ -8,6 +8,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @EnableDiscoveryClient
 public class UrdegoGatewayServiceApplication {
 
+    
     public static void main(String[] args) {
         SpringApplication.run(UrdegoGatewayServiceApplication.class, args);
     }
