9577b854ccb17f59becb0d7e0813fd0b521d0cbe
JoungWoo Lee <j_ra1n@naver.com>
2025-04-07 17:16:10 +0900

    Refactor: Dockerfile 수정

--- src/main/java/io/urdego/urdego_gateway_service/UrdegoGatewayServiceApplication.java
+++ src/main/java/io/urdego/urdego_gateway_service/UrdegoGatewayServiceApplication.java
@@ -7,7 +7,8 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @SpringBootApplication
 @EnableDiscoveryClient
 public class UrdegoGatewayServiceApplication {
-    
+
+
     public static void main(String[] args) {
         SpringApplication.run(UrdegoGatewayServiceApplication.class, args);
     }
